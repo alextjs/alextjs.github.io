84b1cf63bb1749b964e4f843fe3fbe4cd87ec85a
alextjs <75456068+alextjs@users.noreply.github.com>
2021-05-05 20:24:02 +0100

    Add files via upload

---
+++ green-project/js/script.js
@@ -0,0 +1,20 @@
+// let listArray = [
+//     'шапка',
+//     'о нас',
+//     'контакты',
+// ]
+
+// const fsu = document.querySelector('#footer_sign_up')
+
+// listArray.forEach(e=>{
+//     let li = document.createElement('li')
+//     li.innerHTML(e)
+//     li.classList.add('item_list')
+
+//     fsu.insertAdjacentElement('beforebegin', li)
+// })
+
+// for(let i = 0; i <= listArray.length; i++){
+//     let e = listArray[i]
+// }
+
