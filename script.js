cb4a6d2ebe80445250375286e9f36ff134950299
alextjs <75456068+alextjs@users.noreply.github.com>
2021-07-01 22:14:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function() {
     
     // Timer
 
-    const deadline = '2021-05-29';
+    const deadline = '2021-05-30';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -93,5 +93,233 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('.timer', deadline);
+
+
+       
+    
+    
+    
+    
+    
+    // Modal
+
+        const modalTrigger = document.querySelectorAll('[data-modal]'),
+                modal = document.querySelector('.modal');
+
+
+                modalTrigger.forEach(btn => {
+                    btn.addEventListener('click', openModal);
+                });
+
+                function openModal () {
+                        modal.classList.add('show'),
+                        modal.classList.remove('hide'),
+                        document.body.style.overflow = 'hidden';
+                }
+
+                function closeModal () {
+                modal.classList.add('hide'),
+                modal.classList.remove('show'),
+                document.body.style.overflow = '';
+                }
+
+            
+
+
+           modal.addEventListener('click', (e) => {
+            if (e.target === modal || e.target.getAttribute('data-close') == '' ) {
+                closeModal();
+            }
+
+           });
+
+           document.addEventListener('keydown', (e) => {
+            if (e.code === 'Escape' && modal.classList.contains('show')) {
+                closeModal();
+            }
+
+           });
+               
+           const modalTimerId = setTimeout(openModal, 50000);
+
+           showModalByScroll();
+           function showModalByScroll() {
+               if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+                   openModal();
+                   window.removeEventListener('scroll', showModalByScroll(1));
+               }
+           }
+
+           
+           window.addEventListener('scroll', showModalByScroll);
+
+           // Использоваем классы для карточек.
+           class MenuCard {
+            constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+                this.src = src;
+                this.alt = alt;
+                this.title = title;
+                this.descr = descr;
+                this.price = price;
+                this.classes = classes;
+                this.parent = document.querySelector(parentSelector);
+                this.transfer = 27;
+                this.changeToUAH(); 
+            }
+    
+            changeToUAH() {
+                this.price = this.price * this.transfer; 
+            }
+    
+            render() {
+                const element = document.createElement('div');
+    
+                if (this.classes.length === 0) {
+                    this.classes = "menu__item";
+                    element.classList.add(this.classes);
+                } else {
+                    this.classes.forEach(className => element.classList.add(className));
+                }
+    
+                element.innerHTML = `
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                `;
+                this.parent.append(element);
+            }
+        }
+    
+        new MenuCard(
+            "img/tabs/vegy.jpg",
+            "vegy",
+            'Меню "Фитнес"',
+            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            9,
+            ".menu .container"
+        ).render();
+    
+        new MenuCard(
+            "img/tabs/post.jpg",
+            "post",
+            'Меню "Постное"',
+            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            14,
+            ".menu .container"
+        ).render();
+    
+        new MenuCard(
+            "img/tabs/elite.jpg",
+            "elite",
+            'Меню “Премиум”',
+            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            21,
+            ".menu .container"
+        ).render();
+
+
+        // Forms
+
+        const forms = document.querySelectorAll('form');
+
+        const message = {
+            loading: 'img/form/spinner.svg',
+            succes: 'Спасибо! Скоро мы с вами свяжемся обязательно',
+            failure: 'Что-то пошло не так...'
+        };
+
+        forms.forEach ( (item) => {
+            bindPostData(item);
+        });
+
+        const postData = async (url, data) => {
+            let res = await fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: data
+            });
+        
+            return await res.json();
+        };
+
+
+        function bindPostData(form) {
+            form.addEventListener('submit'  , (e) => {
+                e.preventDefault();
+
+                const statusMessage = document.createElement('img');
+                statusMessage.src = message.loading;
+                statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+                `;
+                
+                form.insertAdjacentElement('afterend',statusMessage);
+
+                const request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-type', 'application/json');
+
+                const formData = new FormData(form);
+
+                const json = JSON.stringify(Object.fromEntries(formData.entries()));
+                
+                postData('http://localhost:3000/requests', JSON.stringify(json))
+                .then( data => {
+                    console.log(data);
+                   showThanksModal(message.succes);
+                   statusMessage.remove();
+                })
+                .catch ( () => {
+                    showThanksModal(message.failure);
+                })
+                .finally ( () => {
+                    form.reset();
+                })
+         
+            });
+        }
+
+
+
+        function showThanksModal (message) {
+
+            const prevModalDialog = document.querySelector('.modal__dialog');
+
+            prevModalDialog.classList.add('hide');
+            openModal();
+
+            const thanksModal = document.createElement('div');
+            thanksModal.classList.add('modal__dialog');
+
+            thanksModal.innerHTML = `
+             <div class = "modal__content">
+                <div class = "modal__close" data-close>×</div>
+                <div class = "modal__title">${message}</div>
+             </div>
+            `;
+
+            document.querySelector('.modal').append(thanksModal);
+            setTimeout(() => {
+                thanksModal.remove();
+                prevModalDialog.classList.add('show');
+                prevModalDialog.classList.remove('hide');
+                closeModal();
+            }, 4000)
+
+        }
+
+        fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+    
+
 
 });
