cbac10d6e97c368c50b5c9a8b42767a6d76d69c8
alextjs <75456068+alextjs@users.noreply.github.com>
2021-09-27 12:51:57 +0300

    Add files via upload

---
+++ Mercedes website/js/smoothScroll.js
@@ -0,0 +1,36 @@
+const linksHead = document.querySelectorAll('.menu-list__link')
+const mainScroll = document.querySelector('.main__scroll')
+const newArray = [...linksHead,mainScroll]
+
+
+newArray.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault()
+
+    const ID = event.target.getAttribute('href').substr(1)
+
+    document.getElementById(ID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+    })
+
+    })
+
+})
+
+mainScroll.addEventListener('click',() => {
+
+    document.getElementById('form_contact').scrollIntoView({
+        behavior: 'smooth',
+        block: 'end'
+    })
+
+    })
+
+console.log(mainScroll)
+
+
+
+
+
+
